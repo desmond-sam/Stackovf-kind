83b9872470820aea296fec510c7a526736f6e113
desmond-sam <51733295+desmond-sam@users.noreply.github.com>
2019-08-16 22:02:04 +0530

    Update main.php

--- p2/main.php
+++ p2/main.php
@@ -4,7 +4,7 @@
 <body>
 
 
-
+	<button onclick="location.href = 'anon/home.php';"> Home </button>
 	<button onclick="location.href = 'login/login.php';"> Sign In </button>
 	<button onclick="location.href = 'signup/signup.php';"> Sign Up </button>
 
